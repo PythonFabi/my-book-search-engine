03ae7f0efdabaabf4cf02f8a126bd857fb50f019
PythonFabi <fabianmelone.fw@gmail.com>
2023-09-22 22:26:07 -0400

    import useQuery and Mutations, GETMe query to savedBook.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -7,9 +7,12 @@ import {
   Col
 } from 'react-bootstrap';
 
+import { useQuery, useMutation } from '@apollo/client';
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+
+import { GET_ME } from '../utils/queries';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
