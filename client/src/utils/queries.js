c3e611fae9aae3b626720fde0175145a85eb2394
PythonFabi <fabianmelone.fw@gmail.com>
2023-09-22 20:51:02 -0400

    add the me query in the client

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+// use query to get the current User
+   query me {
+    me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+   }`
