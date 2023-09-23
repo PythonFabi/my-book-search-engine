32a519a13302923fa44bb9bb02d74b3fe3b62a69
PythonFabi <fabianmelone.fw@gmail.com>
2023-09-22 21:44:53 -0400

    Create an ApolloProvider in App.js and format documents with prettier

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,14 +1,14 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 // execute loginUser mutation set up using Apollo Server
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-        token
-        user {
-            _id
-            username
-        }
+      token
+      user {
+        _id
+        username
+      }
     }
   }
 `;
@@ -17,33 +17,49 @@ export const LOGIN_USER = gql`
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
-        token
-        user {
-            _id
-            username
-        }
+      token
+      user {
+        _id
+        username
+      }
     }
   }
 `;
 
 // execute saveBook mutation set up using Apollo Server
 export const SAVE_BOOK = gql`
-  mutation saveBook($authors: [String], $description: String!, $title: String!, $bookId: String!, $image: String, $link: String) {
-    saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link) {
-        user {
-            _id
-            username
-        }
+  mutation saveBook(
+    $authors: [String]
+    $description: String!
+    $title: String!
+    $bookId: String!
+    $image: String
+    $link: String
+  ) {
+    saveBook(
+      authors: $authors
+      description: $description
+      title: $title
+      bookId: $bookId
+      image: $image
+      link: $link
+    ) {
+      user {
+        _id
+        username
+      }
     }
-}`
+  }
+`;
 
 // execute removeBook mutation set up using Apollo Server
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookdId: ID!) {
     removeBook(bookId: $bookId) {
-        user {
-            _id
-            username
-        }
+      user {
+        _id
+        username
+      }
     }
-}`
+  }
+`;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const GET_ME = gql`
 // use query to get the current User
@@ -17,4 +17,4 @@ export const GET_ME = gql`
             link
         }
     }
-   }`
+   }`;
