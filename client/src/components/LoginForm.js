e408d1352cb423baf1b3a6d4e5fab24e1484fdef
PythonFabi <fabianmelone.fw@gmail.com>
2023-09-24 23:46:15 -0400

    replace loginUser with LOGIN_USER mutation

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,14 +1,17 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [loginUser, {error}] = useMutation(LOGIN_USER);
+
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -25,15 +28,18 @@ const LoginForm = () => {
       event.stopPropagation();
     }
 
+    // call loginUser function with userFormData
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await loginUser({
+        variables: {...userFormData},
+      });
 
-      if (!response.ok) {
+      // check for mutation errors
+      if (error) {
         throw new Error('something went wrong!');
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
+      const { token } = await data.loginUser;
       Auth.login(token);
     } catch (err) {
       console.error(err);
